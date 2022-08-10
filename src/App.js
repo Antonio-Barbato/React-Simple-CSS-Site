4517be97f916f4890b5de2f82a274ae2f0f5e053
AntonioBarbato <antonio.barbato95@gmail.com>
2022-08-10 09:17:40 +0200

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import Products from './components/pages/Products';
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/sign-up' element={<SignUp/>}/>
+          <Route path='/products' element={<Products/>}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
